e28db0de3b1a98be5309527ae08e93ddc3acd40c
Robert Verkuil <robert.verkuil@gmail.com>
2018-12-20 19:57:52 -0500

    gitignore update to get rid of unnecessary build directories

--- cpp/drake+pytorch/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rverkuil/integration/drake-pytorch/cpp/drake+pytorch
-# Build directory: /home/rverkuil/integration/drake-pytorch/cpp/drake+pytorch/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- cpp/drake+pytorch/build/src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rverkuil/integration/drake-pytorch/cpp/drake+pytorch/src
-# Build directory: /home/rverkuil/integration/drake-pytorch/cpp/drake+pytorch/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("pytorch_alone")
-subdirs("pytorch_acrobot")
