32e2b39dbece1b6fd9f0d4fe77c76326d61afc46
Robert Verkuil <robert.verkuil@gmail.com>
2019-01-01 11:33:00 -0500

    Finished packaging NNInferenceHelper and FC and MLP nets into NNSystemHelper

--- python/NNSystemHelper.py
+++ python/NNSystemHelper.py
@@ -1,6 +1,10 @@
 import numpy as np
 import sys
+
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 from pydrake.all import (
     AutoDiffXd,
@@ -10,8 +14,39 @@ from pydrake.all import (
 )
 from pydrake.systems.scalar_conversion import TemplateSystem
 
+class FC(nn.Module):
+    def __init__(self, layer_norm=False):
+        super(FC, self).__init__()
+        self.fc1 = nn.Linear(2, 1)
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        return x
 
-def NNInferenceHelper(network, in_np_data):
+class MLP(nn.Module):
+    def __init__(self, layer_norm=False):
+        super(MLP, self).__init__()
+        self.layer_norm = layer_norm
+
+        self.l1 = nn.Linear(2, 64)
+        self.ln1 = nn.LayerNorm(64)
+        self.tanh1 = torch.tanh
+        self.l2 = nn.Linear(64, 64)
+        self.ln2 = nn.LayerNorm(64)
+        self.tanh2 = torch.tanh
+        self.l3 = nn.Linear(64, 1)
+    
+    def forward(self, x):
+        x = self.l1(x)
+        if self.layer_norm: x = self.ln1(x)
+        x = self.tanh1(x)
+        x = self.l2(x)
+        if self.layer_norm: x = self.ln2(x)
+        x = self.tanh2(x)
+        x = self.l3(x)
+        return x
+
+def NNInferenceHelper(network, in_list, debug=False):
     '''
     u (inputs) --> NN => y (outputs)
                    ^
@@ -23,17 +58,17 @@ def NNInferenceHelper(network, in_np_data):
 #
 #    # Convert input to a torch tensor
 #    print("drake_in: ", drake_in)
-#    n_inputs = drake_in.size()
 #    # import pdb; pdb.set_trace()
 
-    just_values = np.array([in_np_data.value() for i in range(n_inputs)])
+    n_inputs = len(in_list)
+    just_values = np.array([item.value() for item in in_list])
     torch_in = torch.tensor(just_values, requires_grad=True)
-    print("torch_in: ", torch_in) 
+    if debug: print("torch_in: ", torch_in) 
 
     # Run the forward pass.
     # We'll do the backward pass(es) when we calculate output and it's gradients.
     torch_out = network.forward(torch_in)
-    print("torch_out: ", torch_out)
+    if debug: print("torch_out: ", torch_out)
     
     # Currently we only support one output
     assert torch_out.shape[0] == 1, "Need just one output for valid cost function"
@@ -43,17 +78,17 @@ def NNInferenceHelper(network, in_np_data):
     # Instead need to follow the advice here:
     #     https://discuss.pytorch.org/t/clarification-using-backward-on-non-scalars/1059
     n_outputs = torch_out.shape[0]
-    out_np_data = [0]*n_outputs
+    out_list = [0]*n_outputs
 
     for j in range(n_outputs):
-        print("\niter j: ", j)
+        if debug: print("\niter j: ", j)
         y_j_value = torch_out[j].clone().detach().numpy()
         # Equation: y.derivs = dydu*u.derivs() + dydp*p.derivs()
         # Alternate equation, for each y, y_j.deriv = sum_i  (dy_jdu[i] * u[i].deriv)
         #                          (#y's) (1x#derivs) (#u's)  (1x#u's)    (1x#derivs)
 
         # Make empty accumulator
-        y_j_deriv = np.zeros_like(in_np_data[0].derivatives())
+        y_j_deriv = np.zeros_like(in_list[0].derivatives())
         
         # https://discuss.pytorch.org/t/clarification-using-backward-on-non-scalars/1059
         output_selector = torch.zeros(1, n_outputs, dtype=torch.double)
@@ -61,15 +96,15 @@ def NNInferenceHelper(network, in_np_data):
         torch_out.backward(output_selector, retain_graph=True)
         dy_jdu = torch_in.grad.numpy() # From Torch, give us back a numpy object
         for i in range(n_inputs):
-            u_i_deriv = in_np_data[i].derivatives()
-            print("dy_jdu_a[i] * u_i_deriv = ", dy_jdu[i], " * ",  u_i_deriv)
+            u_i_deriv = in_list[i].derivatives()
+            if debug: print("dy_jdu_a[i] * u_i_deriv = ", dy_jdu[i], " * ",  u_i_deriv)
             y_j_deriv += dy_jdu[i] * u_i_deriv;
-        print("putting into output: ", y_j_value, ", ", y_j_deriv)
+        if debug: print("putting into output: ", y_j_value, ", ", y_j_deriv)
         tmp = AutoDiffXd(y_j_value, y_j_deriv)
-        out_np_data[j] = tmp
+        out_list[j] = tmp
 
         # output.SetAtIndex(j, tmp)
 
-    return out_np_data
+    return out_list
 
 
--- python/NNTestGradients.py
+++ python/NNTestGradients.py
@@ -1,80 +1,45 @@
-# High-level imports test
-import pydrake
-import pydrake.autodiffutils
-from pydrake.autodiffutils import AutoDiffXd
+from __future__ import print_function, absolute_import
 
-import torch
 import numpy as np
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 torch.manual_seed(1)
 
-from NNSystem import NNSystem, NNSystem_
-from NNTestSetup import NNTestSetup
-
-# Giving NNSystem a pytorch net.
-class FC(nn.Module):
-    def __init__(self, layer_norm=False):
-        super(FC, self).__init__()
-        self.fc = nn.Linear(2, 1)
-    
-    def forward(self, x):
-        x = self.fc(x)
-        return x
-
-class MLP(nn.Module):
-    def __init__(self, layer_norm=False):
-        super(MLP, self).__init__()
-        self.layer_norm = layer_norm
-
-        self.l1 = nn.Linear(2, 64)
-        self.ln1 = nn.LayerNorm(64)
-        self.tanh1 = F.tanh
-        self.l2 = nn.Linear(64, 64)
-        self.ln2 = nn.LayerNorm(64)
-        self.tanh2 = F.tanh
-        self.l3 = nn.Linear(64, 1)
-    
-    def forward(self, x):
-        x = self.l1(x)
-        if self.layer_norm: x = self.ln1(x)
-        x = self.tanh1(x)
-        x = self.l2(x)
-        if self.layer_norm: x = self.ln2(x)
-        x = self.tanh2(x)
-        x = self.l3(x)
-        return x
-    
-# nn_test_setup = NNTestSetup(pytorch_nn_object=MLP())
-# nn_test_setup.RunSimulation()
-
+import pydrake
+import pydrake.autodiffutils
+from pydrake.autodiffutils import AutoDiffXd
 from pydrake.systems.framework import (
     AbstractValue,
     BasicVector, BasicVector_
 )
 
+from NNSystem import NNSystem, NNSystem_
+from NNTestSetup import NNTestSetup
+from NNSystemHelper import FC, MLP
+
+# nn_test_setup = NNTestSetup(pytorch_nn_object=MLP())
+# nn_test_setup.RunSimulation()
+
+# Define network, input
 net = FC()
 net = MLP()
 autodiff_in = np.array([
     [AutoDiffXd(1.0, [1.0, 0.0]),],        
     [AutoDiffXd(1.0, [1.0, 0.0]),]
 ])
-print(type(autodiff_in[0]))
+
+# Make system and give input
 nn_system = NNSystem_[AutoDiffXd](pytorch_nn_object=net)
 context = nn_system.CreateDefaultContext()
-autodiff_in = np.array([AutoDiffXd(x) for x in [1., 2.]])
-# autodiff_in = BasicVector([1., 2.])
 context.FixInputPort(0, autodiff_in)
 
+# Allocate and Eval output
 output = nn_system.AllocateOutput()
 nn_system.CalcOutput(context, output)
-# import pdb; pdb.set_trace()
-value = output.get_vector_data(0).CopyToVector()
-# _get_value_copy() # Might want to use this line above ^
-print("output: ", value)
-
-
-
+autodiff = output.get_vector_data(0).CopyToVector()[0]
+print("autodiff: ", autodiff)
+print("derivatives: ", autodiff.derivatives())
 
