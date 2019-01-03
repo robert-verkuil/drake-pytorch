bbfc1f92d7d6b8cfe9deea5cc9bbc0b6000a9e77
Robert Verkuil <robert.verkuil@gmail.com>
2019-01-03 00:12:49 -0500

    Used value iteration code to create policy visualizer helper scripts

--- python/traj/dircol.py
+++ python/traj/dircol.py
@@ -113,8 +113,8 @@ state_to_tip_coord_fns = {
 }
 
 
-def visualize_trajectory(sample_times, values, expmt=None, create_figure=True):
-    assert expmt is not None
+def visualize_trajectory(sample_times, values, expmt, create_figure=True):
+    assert expmt in ("acrobot", "cartpole")
     if create_figure:
         plt.figure()
         plt.title('Tip trajectories')
@@ -127,8 +127,8 @@ def visualize_trajectory(sample_times, values, expmt=None, create_figure=True):
      
 # Get rid of need for global vars here, if possible?
 vis_cb_counter = 0
-def add_visualization_callback(prog, expmt=None):
-    assert expmt is not None
+def add_visualization_callback(prog, expmt):
+    assert expmt in ("acrobot", "cartpole")
 
     plt.figure()
     plt.title('Tip trajectories')
@@ -142,7 +142,7 @@ def add_visualization_callback(prog, expmt=None):
         if vis_cb_counter % 50 != 0:
             return
         
-        visualize_trajectory(sample_times, values, expmt=expmt, create_figure=False)
+        visualize_trajectory(sample_times, values, expmt, create_figure=False)
 
     print(id(dircol))
     prog.AddStateTrajectoryCallback(MyVisualization)
@@ -193,4 +193,80 @@ def add_running_custom_cost_fn(prog, mycost):
     # Apply our custom PyFunction cost to every timestep of the mathematical program.
     for t in range(21): #TODO: undo the hard coding here
         prog.AddCost(custom_cost_fn, np.hstack((prog.state(t), prog.input(t))))
+
+
+# Given a policy system, (e.g. created by FittedValueIteraton or nn_system.NNSystem(pytorch_net_object))
+# creates a simulator that can be used to visualize the policy.
+from pydrake.all import (
+    DiagramBuilder,
+    FloatingBaseType,
+    RigidBodyTree,
+    RigidBodyPlant,
+    SignalLogger, 
+    Simulator, 
+    VectorSystem
+)
+tree = None
+logger = None
+def visualize_policy_system(policy_system, expmt):
+    global tree
+    global logger
+    expmt_settings = {
+        "acrobot": {
+            'path': "/opt/underactuated/src/acrobot/acrobot.urdf",
+            'state_dim': 4,
+            'twoPI_boundary_condition_state_idxs': (0, 1),
+            'initial_state': [0.5, 0.5, 0.0, 0.0],
+        },
+        "cartpole": {
+            'path': "/opt/underactuated/src/cartpole/cartpole.urdf",
+            'state_dim': 4,
+            'twoPI_boundary_condition_state_idxs': (1,),
+            'initial_state': [0.5, 0.5, 0.0, 0.0],
+        },
+    }
+    assert expmt in expmt_settings.keys()
+
+    # Animate the resulting policy.
+    settings = expmt_settings[expmt]
+    builder = DiagramBuilder()
+    path = settings['path']
+    tree = RigidBodyTree(path, FloatingBaseType.kFixed)
+    plant = RigidBodyPlant(tree)
+    plant_system = builder.AddSystem(plant)
+
+
+    # TODO(russt): add wrap-around logic to barycentric mesh
+    # (so the policy has it, too)
+    class WrapTheta(VectorSystem):
+        def __init__(self):
+            VectorSystem.__init__(self, settings['state_dim'], settings['state_dim'])
+
+        def _DoCalcVectorOutput(self, context, input, state, output):
+            output[:] = input
+            twoPI = 2.*math.pi
+            for idx in settings['twoPI_boundary_condition_state_idxs']:
+                output[idx] = output[idx] - twoPI * math.floor(output[idx] / twoPI)
+
+
+    wrap = builder.AddSystem(WrapTheta())
+    builder.Connect(plant_system.get_output_port(0), wrap.get_input_port(0))
+    vi_policy = builder.AddSystem(policy_system)
+    builder.Connect(wrap.get_output_port(0), vi_policy.get_input_port(0))
+    builder.Connect(vi_policy.get_output_port(0), plant_system.get_input_port(0))
+
+    logger = builder.AddSystem(SignalLogger(settings['state_dim']))
+    logger._DeclarePeriodicPublish(0.033333, 0.0)
+    builder.Connect(plant_system.get_output_port(0), logger.get_input_port(0))
+
+    diagram = builder.Build()
+    simulator = Simulator(diagram)
+    simulator.set_publish_every_time_step(False)
+
+    state = simulator.get_mutable_context().get_mutable_continuous_state_vector()
+    state.SetFromVector(settings['initial_state'])
+
+    return simulator, tree, logger
+
+
 
