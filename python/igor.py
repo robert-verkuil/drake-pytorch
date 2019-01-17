f100131630d7d5b789e1c4bfd42558fafc06bba2
Robert Verkuil <robert.verkuil@gmail.com>
2019-01-16 21:58:07 -0500

    Added in GPU support for the supervised learning

--- python/igor.py
+++ python/igor.py
@@ -99,6 +99,7 @@ def igor_traj_opt_parallel(do_dircol_fn, ic_list, **kwargs):
 
 
 def igor_supervised_learning(trajectories, net, use_prox=True, iter_repeat=1, EPOCHS=1, lr=1e-2):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     alpha, lam, eta = 10., 10.**2, 10.**-2
     frozen_parameters = [param.clone() for param in net.parameters()]
 
@@ -145,6 +146,60 @@ def igor_supervised_learning(trajectories, net, use_prox=True, iter_repeat=1, EP
             running_loss = 0.0
     print('Finished Training')
 
+def igor_supervised_learning_cuda(trajectories, net, use_prox=True, iter_repeat=1, EPOCHS=1, lr=1e-2):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net.to(device)
+
+    alpha, lam, eta = 10., 10.**2, 10.**-2
+    frozen_parameters = [param.clone() for param in net.parameters()]
+
+    criterion1 = nn.MSELoss()
+    criterion2 = nn.MSELoss()
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+
+    # My data
+    all_inputs = np.vstack([np.expand_dims(traj[1], axis=0) for traj in trajectories])
+    all_labels = np.vstack([np.expand_dims(traj[2], axis=0) for traj in trajectories])
+    all_inputs = all_inputs.reshape(-1, all_inputs.shape[-1])
+    all_labels = all_labels.reshape(-1, all_labels.shape[-1])
+    print(all_inputs.shape)
+    print(all_labels.shape)
+
+    inputs = torch.tensor(all_inputs)
+    labels = torch.tensor(all_labels)
+    inputs, labels = inputs.to(device), labels.to(device)
+    #def my_gen():
+    #    for _ in range(iter_repeat):
+    #        yield all_inputs, all_labels
+
+    for epoch in range(EPOCHS):
+        running_loss = 0.0
+        # for i, data in enumerate(my_gen(), 0):
+        for i, _ in enumerate(range(iter_repeat)):
+            # Unpack data
+            #inputs, labels = data
+            # inputs = torch.tensor(inputs)
+            # labels = torch.tensor(labels)
+
+            # Forward pass = THE SAUCE!
+            outputs = net(inputs)
+            loss = alpha/2*criterion1(outputs, labels)
+            if use_prox:
+                for param, ref in zip(net.parameters(), frozen_parameters):
+                    loss += eta/2*criterion2(param, ref)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if (i+1) % iter_repeat == 0:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+    print('Finished Training')
     
 def visualize_intermediate_results(trajectories, 
                                    num_trajectories,
