5236d637e17e7ffc7931f20cc5de82ca5ae0de19
Robert Verkuil <robert.verkuil@gmail.com>
2019-01-19 16:51:12 -0500

    Added dropout options for my commonly used networks

--- python/nn_system/networks.py
+++ python/nn_system/networks.py
@@ -19,57 +19,69 @@ class FC(nn.Module):
         return x
 
 class FCBIG(nn.Module):
-    def __init__(self, n_inputs=4, h_sz=8):
+    def __init__(self, n_inputs=4, h_sz=8, dropout=False):
         super(FCBIG, self).__init__()
         self.n_inputs = n_inputs
-        self.fc2 = nn.Linear(self.n_inputs, h_sz)
-        self.fc3 = nn.Linear(h_sz, 1)
+        self.dropout  = dropout
+
+        self.fc1 = nn.Linear(self.n_inputs, h_sz)
+        self.drop1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(h_sz, 1)
 
     def forward(self, x):
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc1(x))
+        if self.dropout: x = self.drop1(x)
+        x = self.fc2(x)
         return x
 
 class MLPSMALL(nn.Module):
-    def __init__(self, n_inputs=4, layer_norm=False):
+    def __init__(self, n_inputs=4, layer_norm=False, dropout=False):
         super(MLPSMALL, self).__init__()
-        self.n_inputs = n_inputs
+        self.n_inputs   = n_inputs
         self.layer_norm = layer_norm
+        self.dropout    = dropout
 
         h_sz = 16
         self.l1 = nn.Linear(self.n_inputs, h_sz)
         self.ln1 = nn.LayerNorm(h_sz)
         self.tanh1 = torch.tanh
+        self.drop1 = nn.Dropout(0.5)
         self.l3 = nn.Linear(h_sz, 1)
     
     def forward(self, x):
         x = self.l1(x)
         if self.layer_norm: x = self.ln1(x)
         x = self.tanh1(x)
+        if self.dropout: x = self.drop1(x)
         x = self.l3(x)
         return x
 
 class MLP(nn.Module):
-    def __init__(self, n_inputs=4, h_sz=256, layer_norm=False):
+    def __init__(self, n_inputs=4, h_sz=256, layer_norm=False, dropout=False):
         super(MLP, self).__init__()
-        self.n_inputs = n_inputs
+        self.n_inputs   = n_inputs
         self.layer_norm = layer_norm
+        self.dropout    = dropout
 
         self.l1 = nn.Linear(self.n_inputs, h_sz)
         self.ln1 = nn.LayerNorm(h_sz)
         self.tanh1 = torch.tanh
+        self.drop1 = nn.Dropout(0.5)
         self.l2 = nn.Linear(h_sz, h_sz)
         self.ln2 = nn.LayerNorm(h_sz)
         self.tanh2 = torch.tanh
+        self.drop2 = nn.Dropout(0.5)
         self.l3 = nn.Linear(h_sz, 1)
     
     def forward(self, x):
         x = self.l1(x)
         if self.layer_norm: x = self.ln1(x)
         x = self.tanh1(x)
+        if self.dropout: x = self.drop1(x)
         x = self.l2(x)
         if self.layer_norm: x = self.ln2(x)
         x = self.tanh2(x)
+        if self.dropout: x = self.drop2(x)
         x = self.l3(x)
         return x
 
